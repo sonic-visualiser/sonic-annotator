59144e57322aeae0d01dbe37dbb1187e974677cf
Chris Cannam <cannam@all-day-breakfast.com>
2009-01-22 10:58:32 +0000

    * Remove update and co scripts -- unnecessary with externals.  Diff and   commit scripts still needed, however.

--- svn-co.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-svrepo=https://sv1.svn.sourceforge.net/svnroot/sv1/sonic-visualiser/trunk
-
-for x in audioio base data plugin rdf system transform; do
-    svn co $svrepo/$x
-done
-
--- svn-update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-svn update || exit
-
-for x in audioio base data plugin rdf system transform; do
-    svn update $x || exit
-done
-
