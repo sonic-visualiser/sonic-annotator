7ee75fcd93102095ba2b7fab945d9231b23a3f6e
Chris Cannam <cannam@all-day-breakfast.com>
2016-09-15 11:31:23 +0100

    Add test for new fsr-mixed output of Vamp Test Plugin

--- tests/test-vamp-test-plugin/test-vamp-test-plugin.sh
+++ tests/test-vamp-test-plugin/test-vamp-test-plugin.sh
@@ -7,13 +7,16 @@ tmpcsv=$mypath/tmp_1_$$.csv
 
 trap "rm -f $tmpcsv" 0
 
-for output in instants curve-oss curve-fsr curve-fsr-timed curve-vsr grid-oss grid-fsr notes-regions; do
+$r --transform-minversion $testplug 4 || \
+    fail "Vamp Test Plugin version is too old (at least v4 required)"
+
+for output in instants curve-oss curve-fsr curve-fsr-timed curve-fsr-mixed curve-vsr grid-oss grid-fsr notes-regions; do
     
     $r -d "$testplug:$output" -w csv --csv-one-file "$tmpcsv" --csv-force "$infile" 2>/dev/null || \
 	fail "Failed to run for plugin $testplug with output $output"
 
     csvcompare_ignorefirst "$tmpcsv" "$mypath/expected/vamp-test-plugin-$output.csv" || \
-	fail "Output differs from expected for $output"
+	faildiff "Output differs from expected for $output" "$tmpcsv" "$mypath/expected/vamp-test-plugin-$output.csv"
 
 done
 
