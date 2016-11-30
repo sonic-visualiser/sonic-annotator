b966894db35c7bab8e4040f46110415125b8bf1d
Chris Cannam <cannam@all-day-breakfast.com>
2016-11-30 10:16:23 +0000

    More info about where helper programs live

--- tests/test-supportprogs/test-supportprogs.sh
+++ tests/test-supportprogs/test-supportprogs.sh
@@ -6,19 +6,19 @@ fail() {
 }
 
 xmllint --version 2>/dev/null || \
-    fail "Can't find required xmllint program"
+    fail "Can't find required xmllint program (from libxml2 distribution)"
 
 rapper --version >/dev/null || \
-    fail "Can't find required rapper program"
+    fail "Can't find required rapper program (from raptor/redland distribution)"
 
 iconv --version >/dev/null || \
-    fail "Can't find required iconv program"
+    fail "Can't find required iconv program (usually associated with glibc or libiconv)"
 
 echo '{}' | json_verify >/dev/null || \
-    fail "Can't find required json_verify program, or it doesn't seem to work"
+    fail "Can't find required json_verify program (from yajl distribution), or it doesn't seem to work"
 
 echo '{}' | json_reformat >/dev/null || \
-    fail "Can't find required json_reformat program, or it doesn't seem to work"
+    fail "Can't find required json_reformat program (from yajl distribution), or it doesn't seem to work"
 
 exit 0
 
