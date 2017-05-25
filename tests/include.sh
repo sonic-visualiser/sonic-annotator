20ba267dfc0300aa66548217b7b6e9678b926468
Chris Cannam <cannam@all-day-breakfast.com>
2017-05-25 10:24:47 +0100

    Do this for all values, not just first

--- tests/include.sh
+++ tests/include.sh
@@ -34,8 +34,8 @@ csvcompare() {
     # permit some fuzz in final few digits
     a="$1"
     b="$2"
-    perl -p -e 's/(\d+\.\d{6})\d+/$1/' "$a" > "${a}__"
-    perl -p -e 's/(\d+\.\d{6})\d+/$1/' "$b" > "${b}__"
+    perl -p -e 's/(\d+\.\d{6})\d+/$1/g' "$a" > "${a}__"
+    perl -p -e 's/(\d+\.\d{6})\d+/$1/g' "$b" > "${b}__"
     cmp -s "${a}__" "${b}__"
     rv=$?
     rm "${a}__" "${b}__"
