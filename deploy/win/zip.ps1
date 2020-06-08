783576f5efeb33ddbf0defaf66c6f9bc99eac01b
Chris Cannam <cannam@all-day-breakfast.com>
2020-06-08 16:35:31 +0100

    Make sure zipfile is available

--- deploy/win/zip.ps1
+++ deploy/win/zip.ps1
@@ -2,6 +2,8 @@
 Set-StrictMode -Version 2.0
 $ErrorActionPreference = "Stop"
 
+Add-Type -assembly "system.io.compression.filesystem"
+
 if ($args.length -ne 2) {
     echo "Usage: zip target.zip sourcedir"
     exit 2
@@ -19,6 +21,6 @@ if ([System.IO.Path]::IsPathRooted($targetname)) {
 }
 
 echo "Compressing from $source to $target..." | Out-Host
-
+ 
 [io.compression.zipfile]::CreateFromDirectory($source, $target)
 
