821b5b00a10ae4a57bb75ea854fe75da38914e83
Chris Cannam <cannam@all-day-breakfast.com>
2020-06-08 17:12:42 +0100

    Packaging fixes

--- deploy/clean-build-and-package.bat
+++ deploy/clean-build-and-package.bat
@@ -17,6 +17,8 @@ set VERSION=%VERSION:"=%
 call .\deploy\win\build-both.bat
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+@echo on
+
 set NAME=Open Source Developer, Christopher Cannam
 
 @echo Signing 32-bit executables and libraries
@@ -26,7 +28,7 @@ signtool sign /v /n "%NAME%" /t http://time.certum.pl /fd sha1 /a build_win32\re
 signtool sign /v /n "%NAME%" /t http://time.certum.pl /fd sha1 /a build_win64\release\*.exe build_win64\release\*.dll
 
 @echo Zipping up 32-bit package
-set pkg=sonic-annotator-v%VERSION%-win32
+set pkg=sonic-annotator-%VERSION%-win32
 set dir=%pkg%\%pkg%
 mkdir %pkg%
 mkdir %dir%
@@ -34,18 +36,19 @@ copy CHANGELOG %dir%\CHANGELOG.txt
 copy CITATION %dir%\CITATION.txt
 copy COPYING %dir%\COPYING.txt
 copy README.md %dir%\README.txt
-copy build_win32\release\Qt5Core.dll %dir%\
-copy build_win32\release\Qt5Network.dll %dir%\
-copy build_win32\release\Qt5Xml.dll %dir%\
-copy build_win32\release\libgcc_s_dw2-1.dll %dir%\
-copy build_win32\release\libstdc++-6.dll %dir%\
-copy build_win32\release\libwinpthread-1.dll %dir%\
-copy build_win32\release\sonic-annotator.exe %dir%\
+copy build_win32\release\Qt5Core.dll %dir%
+copy build_win32\release\Qt5Network.dll %dir%
+copy build_win32\release\Qt5Xml.dll %dir%
+copy build_win32\release\libgcc_s_dw2-1.dll %dir%
+copy build_win32\release\libstdc++-6.dll %dir%
+copy build_win32\release\libwinpthread-1.dll %dir%
+copy build_win32\release\sonic-annotator.exe %dir%
+
 powershell -NoProfile -ExecutionPolicy Bypass -Command "& 'deploy\win\zip.ps1' %pkg%.zip %pkg%"
 del /q /s %pkg%
 
 @echo Zipping up 64-bit package
-set pkg=sonic-annotator-v%VERSION%-win64
+set pkg=sonic-annotator-%VERSION%-win64
 set dir=%pkg%\%pkg%
 mkdir %pkg%
 mkdir %dir%
@@ -53,11 +56,18 @@ copy CHANGELOG %dir%\CHANGELOG.txt
 copy CITATION %dir%\CITATION.txt
 copy COPYING %dir%\COPYING.txt
 copy README.md %dir%\README.txt
-copy build_win64\release\Qt5Core.dll %dir%\
-copy build_win64\release\Qt5Network.dll %dir%\
-copy build_win64\release\Qt5Xml.dll %dir%\
-copy build_win64\release\libsndfile-1.dll %dir%\
-copy build_win64\release\sonic-annotator.exe %dir%\
+copy build_win64\release\Qt5Core.dll %dir%
+copy build_win64\release\Qt5Network.dll %dir%
+copy build_win64\release\Qt5Xml.dll %dir%
+copy build_win64\release\libsndfile-1.dll %dir%
+copy build_win64\release\sonic-annotator.exe %dir%
+
+copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.25.28508\x64\Microsoft.VC142.CRT\concrt140.DLL" %dir%
+copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.25.28508\x64\Microsoft.VC142.CRT\msvcp140.DLL" %dir%
+copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.25.28508\x64\Microsoft.VC142.CRT\vccorlib140.DLL" %dir%
+copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.25.28508\x64\Microsoft.VC142.CRT\vcruntime140.DLL" %dir%
+copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.25.28508\x64\Microsoft.VC142.CRT\vcruntime140_1.DLL" %dir%
+
 powershell -NoProfile -ExecutionPolicy Bypass -Command "& 'deploy\win\zip.ps1' %pkg%.zip %pkg%"
 del /q /s %pkg%
 
