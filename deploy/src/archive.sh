c324196c484bfdbd977c3a39074d1e219203ed75
Chris Cannam <cannam@all-day-breakfast.com>
2020-06-09 17:32:59 +0100

    Update archive script for Repoint

--- deploy/src/archive.sh
+++ deploy/src/archive.sh
@@ -4,7 +4,28 @@ tag=`hg tags | grep '^sonic-annotator-' | head -1 | awk '{ print $1; }'`
 
 v=`echo "$tag" |sed 's/sonic-annotator-//'`
 
-echo "Packaging up version $v from tag $tag..."
+echo -n "Package up source code for version $v from tag $tag [Yn] ? "
+read yn
+case "$yn" in "") ;; [Yy]) ;; *) exit 3;; esac
+echo "Proceeding"
 
-hg archive -r"$tag" --subrepos --exclude sv-dependency-builds /tmp/sonic-annotator-"$v".tar.gz
+current=$(hg id | awk '{ print $1; }')
 
+case "$current" in
+    *+) echo "ERROR: Current working copy has been modified - unmodified copy required so we can update to tag and back again safely"; exit 2;;
+    *);;
+esac
+          
+echo
+echo -n "Packaging up version $v from tag $tag... "
+
+mkdir -p packages
+
+hg update -r"$tag"
+
+./repoint archive "$(pwd)"/packages/sonic-annotator-"$v".tar.gz --exclude sv-dependency-builds repoint.pri
+
+hg update -r"$current"
+
+echo Done
+echo
