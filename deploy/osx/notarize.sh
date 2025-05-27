8f23b5a58c53041bfe89476eb7d25f22fc0dfa67
Chris Cannam <cannam@all-day-breakfast.com>
2025-05-27 15:59:18 +0100

    Packaging fix

--- deploy/osx/notarize.sh
+++ deploy/osx/notarize.sh
@@ -33,7 +33,7 @@ echo
 echo "Uploading for notarization..."
 
 xcrun notarytool submit \
-    "$dmg" \
+    "$bundle.zip" \
     --apple-id "$user" \
     --team-id "$team_id" \
     --keychain-profile notarytool-cannam \
